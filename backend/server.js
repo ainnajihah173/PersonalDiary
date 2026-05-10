1eac54f86fd0f8c110e270c1c6a9f5810f674af9
ainnajihah173 <134402858+ainnajihah173@users.noreply.github.com>
2026-05-10 08:20:34 +0000

    --

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === 'production') {
 
     // Any route that doesn't match the API, send the index.html
     app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'))
     );
 }
 
